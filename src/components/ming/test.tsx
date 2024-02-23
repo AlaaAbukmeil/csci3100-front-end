76d85a55c3f76824fc16a584653fcb1f23393ea9
chakming chan <chanchakming1@gmail.com>
2024-02-23 15:14:43 +0800

    Add Productlist

---
+++ src/components/ming/test.tsx
@@ -0,0 +1,28 @@
+import  Gadget from './Gadget.jpg' 
+import  Food from './Food.jpg'
+import  Furniture from './Furniture.jpg' 
+import  Cosmetics from '/Cosmetics.jpg' 
+function Test(){
+    let productList =[
+        {"id" : 1, "name" : "Gadget", "price" : 5, "image" : Gadget.jpg, "description" : "testing"},
+        {"id" : 2, "name" : "Food", "price" : 3, "image" : Food.jpg, "description" : "testing"},
+        {"id" : 3, "name" : "Furniture", "price" : 10, "image" : Furniture.jpg, "description" : "testing"},
+        {"id" : 4, "name" : "Cosmetics", "price" : 10, "image" : Cosmetics.jpg, "description" : "testing"}
+    ]
+    return (
+        <div>
+        {
+            productList.map(product=>(
+                <div key = {product.id}>
+                {product.name}<br/>
+                {product.price}<br/>
+                {product.image}<br/>
+                {product.description}<br/>
+                </div>
+            ))
+        }   
+      </div>
+    )
+}
+
+export default Test
