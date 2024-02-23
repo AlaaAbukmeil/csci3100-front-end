72e52800444f28456e632a1c43236d8c82931c4a
Jacky <1155158941@link.cuhk.edu.hk>
2024-02-23 17:27:57 +0800

    Jacky add one file

--- src/components/jacky/test.tsx
+++ src/components/jacky/test.tsx
@@ -1,9 +1,28 @@
+import { useParams} from "react-router-dom";
+import Title from "./Title";
+import QuantityBtn from "./QuantityBtn";
+import { useState } from "react";
+import { useEffect } from "react";
+
 function Test(){
+    let params = useParams();
+    let [productDetail, setProductDetail] = useState(null);
+
+    useEffect(() => {
+        fetch("https://jackycamera.github.io/react-basic-product.json")
+            .then((response) => response.json())
+            .then((data) => {
+                let productInfo = data.find((element: any) => {
+                    return element.id === parseInt(params.id as string);
+                });
+                setProductDetail(productInfo);
+            });
+    }, []);
     return (
-    <div>
-        <h1>Test</h1>
-        <p>Jacky is adding a comment here</p>
-    </div>
+        <div>
+            <Title title="Product Name" />
+            <QuantityBtn productInfo={productDetail} />
+      </div>
     )
 }
 
